9b73500a183d3ebb6c84ce2f7fce8590be1960e2
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2023-02-14 15:16:00 +0100

    Node js learning

--- node.js
+++ node.js
@@ -194,3 +194,53 @@
 // server.listen(PORT, 'localhost', (error) => {
 //     error ? console.log(error) : console.log(`listening port ${3000}`)
 // });
+
+
+// ============================================================
+
+
+// Base Routing
+
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    console.log('request server')
+
+    res.setHeader('Content-type', 'text/html');
+
+    const createPath = (page) => path.resolve(__dirname, 'views', `${page}.html`);
+
+    let basePath = '';
+
+    switch(req.url) {
+        case '/': 
+            basePath = createPath('index');
+            res.statusCode = 200;
+            break;  
+        case '/contact':
+            basePath = createPath('contact');
+            res.statusCode = 200;
+            break;
+        default:
+            basePath = createPath('error');
+            res.statusCode = 404;
+            break;
+    }
+
+    fs.readFile(basePath, (err, data) => {
+        if(err) {
+            res.statusCode = 500;
+            res.end();
+        }
+        else {
+            res.write(data);
+            res.end();
+        }
+    })
+})
+
+server.listen(3000, 'localhost', () => {
+    console.log('listening port 3000')
+})
