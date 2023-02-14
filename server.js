89d7dca1a97ef2a0b1c0bc7f4bd6974e668ee8c5
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2023-02-14 16:01:31 +0100

    Node.js learning

---
+++ server.js
@@ -0,0 +1,19 @@
+// const express = require('express');
+
+// const app = express();
+
+// const PORT = 3000;
+
+
+// app.listen(PORT, (error) => {
+//     error ? console.log('error') : console.log(`listening port ${PORT}`)
+// })
+
+
+// const http = require('http')
+
+// const server = http.createServer((req, res) => {
+//     console.log('request server')
+// })
+
+// server.listen(3000, 'localhost', () => console.log('listening port 3000'))
